e3403e3e39bd83f1e0dec2504d0b8e3ca81c1c73
Emilia <EmiliaDavis2027@gmail.com>
2024-10-24 17:29:55 -0500

    php stuff (i hated this sm)

---
+++ table.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+<?php
+$size = $_POST["size"];
+
+echo "<table>";
+echo "<tr><td></td>";
+for ($i = 1; $i <= $size; $i++) {
+    echo "<td><strong>$i</strong></td>";
+}
+echo "</tr>"
+for ($i = 1; $i <= $size; $i++) {
+    echo "<tr>";
+    echo "<td><strong>$i</strong></td>";
+    for ($j = 1; $j <= $size; $j++) {
+        echo "<td>".($i*$j)."</td>";
+    }
+    echo '<tr>';
+}
+echo "</table>";
+?>
+
+</body>
+</html>
